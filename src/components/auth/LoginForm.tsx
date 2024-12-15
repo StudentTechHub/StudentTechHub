cd778b70d06a0a37f2fb2d52abba9e8404bb4f81
Love Khatri <lovekhatri9211@gmail.com>
2024-12-15 20:21:20 +0530

    refactor: enhance button component structure and icon handling

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -18,209 +18,209 @@ import { LoginSchema } from '@/types/schemas'
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from '@/components/ui/form'
 
 export const LoginForm = () => {
-    const [isPending, startTransition] = useTransition()
+  const [isPending, startTransition] = useTransition()
 
-    const searchParams = useSearchParams()
-    const { toast } = useToast()
+  const searchParams = useSearchParams()
+  const { toast } = useToast()
 
-    const urlError =
-        searchParams.get('error') == 'OAuthAccountNotLinked'
-            ? 'Email already in use with different provider!'
-            : ''
+  const urlError =
+    searchParams.get('error') == 'OAuthAccountNotLinked'
+      ? 'Email already in use with different provider!'
+      : ''
 
-    useEffect(() => {
-        if (urlError) {
+  useEffect(() => {
+    if (urlError) {
+      toast({
+        variant: 'destructive',
+        title: 'Login Error',
+        description: urlError,
+      })
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  const callbackUrl = searchParams.get('callbackUrl')
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const handleSocialLogin = (
+    provider: 'google' | 'github' | 'discord' | 'linkedin'
+  ) => {
+    signIn(provider, { callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT })
+  }
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    startTransition(() => {
+      login(values, callbackUrl)
+        .then((res) => {
+          if (!res?.success && res?.type === 'error') {
+            form.reset()
             toast({
-                variant: 'destructive',
-                title: 'Login Error',
-                description: urlError,
+              variant: 'destructive',
+              title: res.title,
+              description: res.message,
             })
-        }
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-    const callbackUrl = searchParams.get('callbackUrl')
-
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
-        defaultValues: {
-            email: '',
-            password: '',
-        },
-    })
-
-    const handleSocialLogin = (
-        provider: 'google' | 'github' | 'discord' | 'linkedin'
-    ) => {
-        signIn(provider, { callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT })
-    }
-
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-        startTransition(() => {
-            login(values, callbackUrl)
-                .then((res) => {
-                    if (!res?.success && res?.type === 'error') {
-                        form.reset()
-                        toast({
-                            variant: 'destructive',
-                            title: res.title,
-                            description: res.message,
-                        })
-                    }
-                    if (res?.success) {
-                        form.reset()
-                        toast({
-                            variant: 'default',
-                            title: res.title,
-                            description: res.message,
-                        })
-                    }
-                })
-                .catch((error) => {
-                    if (error.message !== 'NEXT_REDIRECT') {
-                        toast({
-                            variant: 'destructive',
-                            title: 'An error occurred',
-                            description: 'Please try again later',
-                        })
-                    }
-                })
+          }
+          if (res?.success) {
+            form.reset()
+            toast({
+              variant: 'default',
+              title: res.title,
+              description: res.message,
+            })
+          }
         })
-    }
+        .catch((error) => {
+          if (error.message !== 'NEXT_REDIRECT') {
+            toast({
+              variant: 'destructive',
+              title: 'An error occurred',
+              description: 'Please try again later',
+            })
+          }
+        })
+    })
+  }
 
-    return (
-        <>
-            <Card className="w-[400px] shadow-md">
-                <CardHeader>
-                    <div className="flex w-full flex-col items-center justify-center gap-y-4">
-                        <h1 className={cn('text-3xl font-semibold')}>🔑Auth</h1>
-                        <p className="text-muted-foreground text-sm">
-                            Welcome Back
-                        </p>
-                    </div>
-                </CardHeader>
-                <CardContent>
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className="space-y-6"
-                        >
-                            <div className="space-y-4">
-                                <FormField
-                                    control={form.control}
-                                    name="email"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Email</FormLabel>
-                                            <FormControl>
-                                                <Input
-                                                    disabled={isPending}
-                                                    {...field}
-                                                    type="email"
-                                                    placeholder="johndoe@mail.com"
-                                                />
-                                            </FormControl>
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                                <FormField
-                                    control={form.control}
-                                    name="password"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Password</FormLabel>
-                                            <FormControl>
-                                                <Input
-                                                    disabled={isPending}
-                                                    {...field}
-                                                    type="password"
-                                                    placeholder="********"
-                                                />
-                                            </FormControl>
-                                            <Button
-                                                size={'sm'}
-                                                variant="link"
-                                                asChild
-                                                className="px-0 font-normal"
-                                            >
-                                                <Link href="/reset-password">
-                                                    Forgot Password?
-                                                </Link>
-                                            </Button>
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                            </div>
-                            <Button
-                                disabled={isPending}
-                                type="submit"
-                                className="w-full"
-                            >
-                                Login
-                            </Button>
-                        </form>
-                    </Form>
-                </CardContent>
-                <CardFooter>
-                    <div className="flex w-full items-center gap-x-2">
-                        <Button
-                            size={'lg'}
-                            className="w-full"
-                            variant={'outline'}
-                            onClick={() => handleSocialLogin('google')}
-                        >
-                            <FaGoogle />
-                        </Button>
-                        <Button
-                            size={'lg'}
-                            className="w-full"
-                            variant={'outline'}
-                            onClick={() => handleSocialLogin('github')}
-                        >
-                            <FaGithub />
-                        </Button>
-                        <Button
-                            size={'lg'}
-                            className="w-full"
-                            variant={'outline'}
-                            onClick={() => handleSocialLogin('linkedin')}
-                        >
-                            <FaLinkedin />
-                        </Button>
-                        <Button
-                            size={'lg'}
-                            className="w-full"
-                            variant={'outline'}
-                            onClick={() => handleSocialLogin('discord')}
-                        >
-                            <FaDiscord />
-                        </Button>
-                    </div>
-                </CardFooter>
-                <CardFooter>
-                    <Button
-                        variant={'link'}
-                        className="w-full font-normal"
+  return (
+    <>
+      <Card className="w-[400px] shadow-md">
+        <CardHeader>
+          <div className="flex w-full flex-col items-center justify-center gap-y-4">
+            <h1 className={cn('text-3xl font-semibold')}>🔑Auth</h1>
+            <p className="text-muted-foreground text-sm">
+              Welcome Back
+            </p>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-6"
+            >
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isPending}
+                          {...field}
+                          type="email"
+                          placeholder="johndoe@mail.com"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isPending}
+                          {...field}
+                          type="password"
+                          placeholder="********"
+                        />
+                      </FormControl>
+                      <Button
                         size={'sm'}
+                        variant="link"
                         asChild
-                    >
-                        <Link href={'/signup'}>
-                            Don&apos;t have an account?
+                        className="px-0 font-normal"
+                      >
+                        <Link href="/reset-password">
+                          Forgot Password?
                         </Link>
-                    </Button>
-                </CardFooter>
-            </Card>
-        </>
-    )
+                      </Button>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button
+                disabled={isPending}
+                type="submit"
+                className="w-full"
+              >
+                Login
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter>
+          <div className="flex w-full items-center gap-x-2">
+            <Button
+              size={'lg'}
+              className="w-full"
+              variant={'outline'}
+              onClick={() => handleSocialLogin('google')}
+            >
+              <FaGoogle />
+            </Button>
+            <Button
+              size={'lg'}
+              className="w-full"
+              variant={'outline'}
+              onClick={() => handleSocialLogin('github')}
+            >
+              <FaGithub />
+            </Button>
+            <Button
+              size={'lg'}
+              className="w-full"
+              variant={'outline'}
+              onClick={() => handleSocialLogin('linkedin')}
+            >
+              <FaLinkedin />
+            </Button>
+            <Button
+              size={'lg'}
+              className="w-full"
+              variant={'outline'}
+              onClick={() => handleSocialLogin('discord')}
+            >
+              <FaDiscord />
+            </Button>
+          </div>
+        </CardFooter>
+        <CardFooter>
+          <Button
+            variant={'link'}
+            className="w-full font-normal"
+            size={'sm'}
+            asChild
+          >
+            <Link href={'/signup'}>
+              Don&apos;t have an account?
+            </Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
-import { Slot } from '@radix-ui/react-slot'
+import { Slot, Slottable } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-border dark:focus-visible:ring-neutral-950 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+    'inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-border dark:focus-visible:ring-neutral-950 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
     {
         variants: {
             variant: {
-                primary: `hover:bg-primary/90 dark:hover:bg-primary/90 disabled:bg-primary/50 bg-primary text-white-0 active:bg-primary-600 dark:bg-primary dark:text-white-0 dark:active:bg-primary-600`,
+                primary: `bg-primary hover:bg-primary/90 dark:hover:bg-primary/90 disabled:bg-primary/50 bg-primary text-white-0 active:bg-primary-600 dark:bg-primary dark:text-white-0 dark:active:bg-primary-600`,
                 secondary:
                     'text-neutral-950 hover:bg-neutral-100 active:bg-neutral-200 disabled:bg-transparent disabled:text-neutral-500 dark:border-neutral-800 dark:text-neutral-950 dark:hover:bg-neutral-100 dark:active:bg-neutral-200',
                 outline:
@@ -20,9 +20,9 @@ const buttonVariants = cva(
                     'bg-success text-neutral-50 hover:bg-success/80 active:bg-success-500 dark:bg-success dark:text-neutral-50 dark:hover:bg-success/90',
             },
             size: {
-                default: 'h-10 rounded-full px-4 py-2.5 text-base',
-                sm: 'h-9 rounded-full px-3 text-sm',
-                lg: 'h-11 rounded-full px-8 text-lg',
+                default: 'h-10 rounded-full px-5 py-3 gap-2 text-base',
+                sm: 'h-9 rounded-full px-4 py-2.5 gap-1.5 text-sm',
+                lg: 'h-11 rounded-full px-6 py-3.5 gap-2.5 text-lg',
                 icon: 'h-10 w-10 rounded-full text-base',
             },
         },
@@ -35,7 +35,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-        VariantProps<typeof buttonVariants> {
+    VariantProps<typeof buttonVariants> {
     asChild?: boolean
     icon?: React.ReactNode
     iconPosition?: 'leading' | 'trailing'
@@ -48,7 +48,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             variant,
             size,
             asChild = false,
-            icon,
+            icon: Icon,
             iconPosition = 'leading',
             ...props
         },
@@ -56,19 +56,26 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ) => {
         const Comp = asChild ? Slot : 'button'
         return (
-            <Comp
-                className={cn(buttonVariants({ variant, size, className }))}
-                ref={ref}
-                {...props}
-            >
-                {icon && iconPosition === 'leading' && (
-                    <span className="icon">{icon}</span>
-                )}
-                {props.children}
-                {icon && iconPosition === 'trailing' && (
-                    <span className="icon">{icon}</span>
-                )}
-            </Comp>
+            <>
+                <Comp
+                    className={cn(buttonVariants({ variant, size, className }))}
+                    ref={ref}
+                    {...props}
+                >
+                    {/* Left Icon */}
+                    {Icon && iconPosition === 'leading' && (
+                        Icon
+                    )}
+                    {/* Text */}
+                    <Slottable>
+                        {props.children}
+                    </Slottable>
+                    {/* Right Icon */}
+                    {Icon && iconPosition === 'trailing' && (
+                        Icon
+                    )}
+                </Comp>
+            </>
         )
     }
 )
